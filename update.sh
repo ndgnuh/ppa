4594584759d250fb8958b6d11a202f17680eeda0
Hung Nguyen Duc <ndgnuh@protonmail.com>
2025-02-15 14:20:41 +0700

    Sat Feb 15 02:20:41 PM +07 2025

--- update.sh
+++ update.sh
@@ -1,9 +1,15 @@
 EMAIL=ndgnuh@protonmail.com
 REPO=debian
 
-dpkg-scanpackages --multiversion "$REPO" > "$REPO/Packages"
-gzip -k -f "$REPO/Packages"
-apt-ftparchive release "$REPO" > "$REPO/Release"
+cd $REPO
 
-gpg --default-key "${EMAIL}" -abs -o - "$REPO/Release" > "$REPO/Release.gpg"
-gpg --default-key "${EMAIL}" --clearsign -o - "$REPO/Release" > "$REPO/InRelease"
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+# Release, Release.gpg & InRelease
+apt-ftparchive release . > Release
+gpg --default-key "${EMAIL}" -abs -o - Release > Release.gpg
+gpg --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+
+cd ..
